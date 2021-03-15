b30bf9629e9ea3c40806065596d11c69209ad4ff
Ian Preglo <nipreglo@gmail.com>
2021-03-15 17:51:02 +0800

    Copied the source code from the demo to this branch. The branch is functioning like the demo already.

--- src/cat.js
+++ src/cat.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link, Redirect, withRouter } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import axios from 'axios';
+
 import './css/cat.scss';
 
-class Cat extends React.Component {
+/**
+ * Cat component
+ */
+class Cat extends Component {
 
-    render() {
-        return (
-            <div>Cat Details</div>
-        );
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      cat: {},
+      ready: false
     }
+  }
+
+  render() {
+    const { cat, ready } = this.state;
+    if (ready && !cat.id) {
+      return (
+        <Redirect to="/" />
+      );
+    }
+    if (!ready) {
+      return (
+        <div className="Cat">
+          <Container>
+            <h5>Loading...</h5>
+          </Container>
+        </div>
+      );
+    }
+    const breed = cat.breeds[0];
+    return (
+      <div className="Cat">
+        <Container>
+          <Card>
+            <Card.Header>
+              <Link className="btn btn-primary" to={'/?breed=' + breed.id}>Back</Link>
+            </Card.Header>
+            <Card.Img src={cat.url} />
+            <Card.Body>
+              <h4>{breed.name}</h4>
+              <h5>Origin: {breed.origin}</h5>
+              <h6>{breed.temperament}</h6>
+              <p>{breed.description}</p>
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    axios('//api.thecatapi.com/v1/images/' + this.props.match.params.id).then(({ data }) => {
+      this.setState({
+        cat: data,
+        ready: true
+      });
+    });
+  }
 }
+
+export default withRouter(Cat);
--- src/home.js
+++ src/home.js
@@ -1,52 +1,148 @@
-import React from 'react';
+import { findIndex, isUndefined } from 'lodash';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import axios from 'axios';
+
 import './css/home.scss';
 
-export class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            catbreeds: [],
-        };
-    }
-
-    componentDidMount() {
-        var fetch_options = {
-            headers: {
-                'method': 'GET',
-                'x-api-key' : '131ee1db-7501-443d-8984-e5f4fdd8b6d5'
-            }
-        };
-
-        fetch('https://api.thecatapi.com/v1/breeds', fetch_options)
-        .then(response => {
-            response.json().then(data => {
-                this.setState({catbreeds: data});
-            });
-        });
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <div className="Home">
-                    <h1>Cat Browser</h1>
-                    <div className="row" style={{padding: '10px 0px'}}>
-                        <div className="col-md-3 col-sm-6 col-12">
-                            <div className="form-group">
-                                <label className="form-label" for="breed">Breed</label>
-                                <select id="breed" class="form-control">
-                                    <option>Select breed</option>
-                                    {this.state.catbreeds.map(breed=>(
-                                        <option value={breed.id}>{breed.name}</option>
-                                    ))}
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+/**
+ * Query string to object
+ */
+export function fromQuery(url) {
+  const question = url.indexOf('?');
+  if (question < 0) {
+    return {};
+  }
+  const object = {},
+        hash = url.indexOf('#', question),
+        query = url.substring(question + 1, (hash < 0) ? undefined : hash);
+  query.split('&').forEach((pair) => {
+    const [name, value] = pair.split('=');
+    object[name] = isUndefined(value) ? true : decodeURIComponent(value);
+  });
+  return object;
 }
+/**
+ * Home component
+ */
+export default class Home extends Component {
 
-export default Home;
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      busy: false,
+      cats: [],
+      breed: '',
+      breeds: [],
+      overflow: false,
+      page: 1,
+      ready: false
+    }
+  }
+
+  render() {
+    const { breed, breeds, busy, cats, overflow, page, ready } = this.state;
+    return (
+      <div className="Home">
+        <Container>
+          <h1>Cat Browser</h1>
+          <Row style={{ padding: '10px 0' }}>
+            <Col md={3} sm={6} xs={12}>
+              <Form.Group controlId="breed">
+                <Form.Label>Breed</Form.Label>
+                <Form.Control disabled={!ready || busy} as="select" value={breed} onChange={(e) => { this.select(e.target.value); }}>
+                  <option value="">Select breed</option>
+                  {breeds.map(({ id, name }) => (
+                    <option key={id} value={id}>{name}</option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            {(!cats.length ?
+              <Col xs={12} style={{ marginBottom: '20px' }}>No cats available</Col> :
+              cats.map(({ id, url }, i) => (
+                <Col md={3} sm={6} xs={12} key={i}>
+                  <Card>
+                    <Card.Img variant="top" src={url} />
+                    <Card.Body>
+                      <Link className="btn btn-primary btn-block" to={'/' + id}>View details</Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))
+            )}
+          </Row>
+          {(overflow ? '' :
+            <Row>
+              <Col md={3} sm={6} xs={12}>
+                <Button variant="success" disabled={!breed || busy} type="button" onClick={() => { this.load(page + 1) }}>
+                  {busy ? 'Loading cats...' : 'Load more'}
+                </Button>
+              </Col>
+            </Row>
+          )}
+        </Container>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    axios.get('//api.thecatapi.com/v1/breeds').then(({ data }) => {
+      this.setState({
+        breeds: data,
+        ready: true
+      });
+      const query = this.props.location ? fromQuery(this.props.location.search) : {};
+      if (query.breed) {
+        this.select(query.breed);
+      }
+    });
+  }
+
+  /**
+   * Select
+   */
+  select(breed) {
+    this.setState({
+      breed,
+      cats: []
+    });
+    if (breed) {
+      this.load(1, breed);
+    }
+  }
+
+  /**
+   * Load
+   */
+  load(page, breed = this.state.breed) {
+    axios(`//api.thecatapi.com/v1/images/search?page=${page}&limit=10&breed_id=${breed}`).then(({ data }) => {
+      const cats = [];
+      data.forEach((cat) => {
+        if (findIndex(this.state.cats, ({ id }) => (id === cat.id)) < 0) {
+          cats.push(cat);
+        }
+      })
+      this.setState({
+        busy: false,
+        cats: [
+          ...this.state.cats,
+          ...cats
+        ],
+        overflow: (cats.length === 0)
+      });
+    });
+    this.setState({
+      busy: true,
+      page
+    });
+  }
+}
